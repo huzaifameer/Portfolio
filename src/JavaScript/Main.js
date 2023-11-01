fec0b399a0892c31dac5ddbaf9bbf6b3c53a16bb
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-11-01 20:42:34 +0530

    committed-day4

--- src/JavaScript/Main.js
+++ src/JavaScript/Main.js
@@ -32,6 +32,28 @@ window.onscroll = () => {
     subNavMin.classList.remove("active");
 };
 
+/*--------Contact Form----------*/
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value === "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+});
+
+/*--------Contact Form----------*/
+
 
 
 

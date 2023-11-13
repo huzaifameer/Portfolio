b493cbcbaaa2d4164a2525ade666ba3d8b6b2632
HuzaifaAmeer <huzaifaameer00@gmail.com>
2023-11-13 20:22:22 +0530

    committed-day13

--- src/JavaScript/Main.js
+++ src/JavaScript/Main.js
@@ -1,9 +1,8 @@
-// Select the navigation bar element
+// Getting the navigation bar element
 const navbar = document.getElementById("navbar");
 const navbarMin = document.querySelector(".navbar");
 const subNavMin = document.querySelector(".p-header");
-
-// Add a scroll event listener
+// Adding a scroll event listener
 window.addEventListener("scroll", function () {
     // Check the scroll position
     if (window.scrollY > 80) { // Change 100 to the desired scroll position
@@ -12,7 +11,6 @@ window.addEventListener("scroll", function () {
         navbar.style.backgroundColor = "transparent"; // Restore the initial background color
     }
 });
-
 // Toggle mobile menu
 document.querySelector("#menu-icon").onclick = () => {
     navbarMin.classList.toggle("active");
@@ -22,7 +20,6 @@ document.querySelector("#menu-icon-2").onclick = () => {
     subNavMin.classList.toggle("active");
     navbarMin.classList.remove("active");
 };
-
 // Close mobile menu on scroll
 window.onscroll = () => {
     navbarMin.classList.remove("active");
